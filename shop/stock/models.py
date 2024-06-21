794851491c2d632db24f34a60bf713e6358c6cd7
Alvaro Escudero <135117878+Alvaroescumed@users.noreply.github.com>
2024-06-21 19:53:06 +0200

    first commit

---
+++ shop/stock/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Category(models.Model):
+    name= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True) 
+
+    def __str__(self):
+        return self.name
+
+
+class Inventory(models.Model):
+
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return f'{self.product.name} -- {self.product.quantity}'
